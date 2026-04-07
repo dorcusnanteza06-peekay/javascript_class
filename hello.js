db7703d148bf3e55aa12807468f0d96503ee0b20
dorcusnanteza06-peekay <dorcusnanteza06@gmail.com>
2026-04-07 08:49:26 +0300

    javascript loops and functions

--- hello.js
+++ hello.js
@@ -6,45 +6,46 @@
 /// Declaring variables    var, let, const
 /// var is function- scoped and can be redeclared and updated
 
-var name = "job";
-console.log(name);
-name = "john";
-console.log (name)
+//var name = "job";
+//console.log(name);
+//name = "john";
+//console.log (name)
 
 //// let//
-let age = 30;
-console.log(age);
-age = 20
-console.log("My age has changed because Iam just a girl", age);
+
+///let age = 30;
+///console.log(age);
+///age = 20
+//console.log("My age has changed because Iam just a girl", age);
 
 /// const is block-scoped and cannot be updated or redeclared
-const height = 5.8;
-console.log(height);
+//const height = 5.8;
+//console.log(height);
 /// height = 6.0/// this would cause an error
 
-const country = "Uganda";
-console.log(country);
+///const country = "Uganda";
+///console.log(country);
 ///country = "kenya"
 ///console.log(kenya);
 
 
 //// Data Types 
 /// String data type 
-let greeting = "Hello, How are you?";      ////string 
-console.log(greeting);
-console.log(typeof greeting); 
+//let greeting = "Hello, How are you?";      ////string 
+//console.log(greeting);
+//console.log(typeof greeting); 
 
 //// Number data type
-let year = 2026;
-console.log(year);
-console.log(typeof year);
+//let year = 2026;
+//console.log(year);
+//console.log(typeof year);
 
-let x = 10;
-let y = "12";          //// this makes it a string 
-console.log (x);
-console.log(y);
-console.log(typeof x);
-console.log(typeof y);
+//let x = 10;
+//let y = "12";          //// this makes it a string 
+//console.log (x);
+//console.log(y);
+//console.log(typeof x);
+//console.log(typeof y);
 
 /// Boolean data type
 // its a conditional statement of true or false
@@ -64,33 +65,228 @@ console.log(a ** b);    ///  exponentiaton
 ////conditionals in Js
 /// if statement
 /// definition of if statement
-if (condition) {
+///if (condition) {
     /// code to be executed if condition is true
-};
+///};
 
 // if else statement 
 // definition of the if-else statement
 
-if (condition){
+///if (condition){
     /// code to be executed if conditions is true
-} else{
+///} else{
     /// code to be executed if the condition is false
-}
+//}
 
 /// else-if statement
 /// definition of else if statement
-if (condition){
+///if (condition1){
     /// code to be executed if conidition 1 is true
-} else if (conidition2){ 
+//} else if (conidition2){ 
     /// code to be executed if condition is true
-} else {
-    /// code to be executed if cndition is false
-}
+//} else if(condition3){
+    /// code to be executed if condition is false
+//} else{
+       /// code to be executed if condition is false
+//}
 
 
 /// practical examples 
-let mark = 75;
+let mark = 30;
 
 if (mark > 69){
-    console.log("Very good, you have passed the exam")
+    console.log("Very good, you have passed the exam");
 }
+else if(mark >= 40){
+    console.log("fair, you have faired the exam");
+}
+else {
+    console.log("poor, you failed the exam");
+}
+
+
+/// Comparsion Operators
+/// 1. Equal to  (**)
+// Eg of equal to operator
+//if(name = "john") /// used to asign a value
+//if (name =="john")   ///this will compare to john and evaluate to true
+
+/// 2. Not Equal to (!=)
+/// 3. Strict egual to (***)
+/// 4. Strict Not Equal to (!**)
+/// 5. Greater than(>)
+/// 6. less than (<)
+/// 7. Greater than or Equal to (>=)
+/// 8. Less than or Equal to(<=)
+
+/// ARRAY
+// its a list of numbers, strings, Booleans, 
+/// is a data structure that can hold a multiple structures 
+// examples of arrays
+//cars = ["volvo","Bez","Toyota","kia"]
+//numbers = [1,2,3,4,5,6]
+//['job',30,'uganda',true,null,undefined]
+
+// example 1
+//var cars = ["volvo","Bez","Toyota","kia"];
+//console.log(cars);
+//console.log (cars[2]);
+
+//example 2 
+//let mixedArray = ['job',30,'Uganda',true,null,undefined,(name = "john")];
+//console.log(mixedArray);
+//console.log(mixedArray[6]);
+
+/// objects
+//{}  // empty object
+//{
+    //name: "job",
+    //age: 30,
+    //country: "USA",
+    //isStudent: 
+//}    /// object with a property called name john
+
+//example 1
+let person = {
+    name: "job",
+    age:30,
+    country:"Uganda",
+    isStudent:true,
+    hobbies:['coding','travelling'],
+    address:{
+        Street:'123 Mainst',
+        City:'Kampala',
+        Country:'Uganda'
+    }
+
+}
+console.log(person);
+console.log(person["name"]);  ///accessing the name property
+console.log (person.hobbies[1])  /// accessing hobbies 
+
+/// forEach
+/// fall back function
+/// control loop
+/// functions
+// functions are reuseable blocks of code that perform a specific task
+// take parameters and return values 
+// function declaration we use the function keyword to declare a function E.g
+
+//function fristName(){
+    /// code 
+    //return parameter;   /// returning the parameter
+//}
+
+//function bold(parameter1,parameter2,parameter3){
+    //return parameter1+parameter2+parameter3;
+//}
+
+/// example of a function
+function returnValue(){
+    let value = "this is my first function in javascript";
+    console.log(value);
+}
+returnValue();     // This is how you call a function to execute the code inside it
+
+// example 2
+function returnParameter(parameter){
+    return parameter;
+}
+///console.log(returnParameter("Hello, this is my second function in javascript"));
+
+ const result = returnParameter("Hello, this is my second function in javascript");
+ console.log(result);
+
+ // example 3  returning the sum
+ function addNumbers(x,y){
+    return x + y;            //// x and y are parameters
+ }
+const sum =addNumbers(5,10);      /// 5 and 10 are arguments 
+console.log (sum);
+
+/// loops in javascript 
+//1. while loop
+//2. Do while Loop
+//3. For Loop
+// example 1
+for (let i = 0; i < 7 ;i++){
+    console.log(i);
+}
+/// example 2 increameting for loop
+for (let i = 7; i > 0; i--){
+    console.log(i);
+}
+
+
+/// example 2 Decreameting for loop
+//for (let i = 7; i > 0; i++){
+    //console.log(i);
+//}
+
+const myCars = ['Volvo','Benz','BMW','Toyota'];
+console.log(myCars);
+const numberOfCars = myCars.length;   // this gives you the number of elements in the array
+const BmwPosition = myCars.indexOf('BMW');
+console.log('postion of the BMW in the array is ',BmwPosition);
+console.log (numberOfCars);
+
+for (let i = 0; i<myCars.length; i++){
+    console.log(myCars[i]);
+}
+
+for(let car of myCars){
+    function printCarPosition(car){
+        const index = myCars.indexOf(car);
+        console.log ('The position of '+car +' in the array is '+ index );
+    }
+    printCarPosition(car);
+    console.log(car);
+}
+/// 2. while Loop
+//while(condition){
+    /// code to be executed as long as the condition is true
+//}
+
+/// Increamenting while loop
+let id =0; 
+while (id < 7){
+    console.log('From the while loop +, id');
+    id++;  
+}
+
+let password = "";
+
+while (password !== "1234"){
+    password = prompt("Please enter the password:");
+}
+
+alert("Access granted!");
+
+
+/// Decreamenting while loop
+
+let ie =10; 
+while (ie > 1){
+    console.log('From the while loop +, ie');
+    ie--;  
+}
+
+/// Do while Loop
+//do{
+    /// code to be executed
+///} while (condition);
+
+///example 
+let i = 0;
+do{
+    console.log ('From the do while loop',i);
+    i++;
+} while (i < 7);
+
+
+  
+
+
+
+
+
--- index.js
+++ index.js
@@ -1,71 +1,25 @@
-const names = "this is Dorcus"
-console.log("This is Dorcus");
+/// Assignment about Javascript 
+let mark = 70;
 
-//// variables ///
-//let name = "joker";
-
-///declaring a variable
-//declaring variables uses var,let or const
-
-//var is function-scoped and can be redeclared and updated 
-var name = "joker";
-console.log("name");
-
-let age = 27
-console.log(age);
-age = 18
-console.log("my age has changed because iam just a girl",age);
-
-////cost is block-scoped and cannot be updated or declared
-const country = "uganda";
-console.log(country);
-country = "kenya"; //This 
-console.log(country)
-
-/// 1. string data type 
-let greetings = "hello, how are you"
-console.log (greeting);
-
-///2. Number data type
-
-let x = 20;
-let y = 40;
-//console.log(x); 
-console.log(y);
-console.log(typeof x);
-console.log(typeof y);
-
-///strings 
-///numbers 
-///booleans 
-/// arrays 
-/// undefined 
-/// js conditionals 
-
-/// js assignment about if function 
-function getGrade(marks) {
-    if (marks >= 80) {
-        return "Distinction";
-    } else if (marks >= 70) {
-        return "First Class";
-    } else if (marks >= 60) {
-        return "Second Class";
-    } else if (marks >= 50) {
-        return "Pass";
-    } else {
-        return "Fail";
-    }
+if (mark >= 90){
+    console.log("Grade A");
 }
-
-// Example usage
-let studentMarks = 75;
-let grade = getGrade(studentMarks);
-
-console.log("Marks: " + studentMarks);
-console.log("Grade: " + grade);
-
-
-//// loops
-
-let scores 
-
+else if (mark >= 80){
+    console.log("Grade B");
+}
+else if (mark >= 70){
+    console.log("Grade C");
+}
+else if (mark >= 60){
+    console.log("Grade D");
+}
+else if (mark >= 50){
+    console.log("Grade E");
+}
+else if (mark >= 40){
+    console.log("Grade F");
+}
+else {
+    console.log("Grade O (Fail)");
+}
+console.log(mark);
